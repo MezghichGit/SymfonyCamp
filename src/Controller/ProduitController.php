99397a4ae25318428a44f168c9d3d0df04edcdbf
moahmed amine mezghich <git@mezghich.com>
2023-04-08 16:47:42 +0100

    crud complet

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -15,7 +15,7 @@ use Doctrine\Persistence\ManagerRegistry;
 class ProduitController extends AbstractController
 {
     #[Route('/', name: 'app_produit_index')]
-    public function index(ProduitRepository $produitRepository): Response
+   public function index(ProduitRepository $produitRepository): Response
     {
         $produits = $produitRepository->findAll();
 
@@ -23,7 +23,7 @@ class ProduitController extends AbstractController
         return $this->render('produit/index.html.twig', [
             'produits' => $produits,
         ]);
-    }
+    } 
 
 
     #[Route('/add', name: 'add')]
@@ -31,10 +31,10 @@ class ProduitController extends AbstractController
     {
         if($request->getMethod()=="POST") {
             $product = new Produit();
-           
+
             $product->setLibelle($request->get("libelle"));
             $product->setPrix($request->get("prix"));
-            
+
             //$errors = $validator->validate($product);
             /*if (count($errors) > 0) {
                 return new Response((string) $errors, 400);
@@ -45,7 +45,7 @@ class ProduitController extends AbstractController
             $produitRepository->save($product, true);
 
             //return new Response('Saved new product with id '.$product->getId());
-            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_produit_index');
         } else {
             return $this->render('produit/add.html.twig');
         }
@@ -64,7 +64,59 @@ class ProduitController extends AbstractController
             );
         }
 
-        return new Response('Check out this great product: '.$produit->getLibelle());
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+        ]);
+        //return new Response('Check out this great product: '.$produit->getLibelle());
+
+    }
+
+
+    #[Route('/delete/{id}', name: 'delete_produit')]
+    public function delete_produit($id, ProduitRepository $produitRepository)
+    {
+
+        $produit = $produitRepository->find($id);
+        if (!$produit) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        $produitRepository->remove($produit, true);
+        return $this->redirectToRoute('app_produit_index');
+
+    }
+
+    #[Route('/editer/{id}', name: 'edit_produit')]
+    public function edit_produit(Request $request, $id, ProduitRepository $produitRepository)
+    {
+
+        $produit = $produitRepository->find($id);
+        if (!$produit) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        if($request->getMethod()=="POST") {
+
+            $produit->setLibelle($request->get("libelle"));
+            $produit->setPrix($request->get("prix"));
+
+            $produit->setDescription($request->get("description"));
+            $produit->setPhoto("image.png");
+            $produitRepository->save($produit, true);
+
+            //return new Response('Saved new product with id '.$product->getId());
+            return $this->redirectToRoute('app_produit_index');
+        } else {
+            //dd($produit->getLibelle());
+            return $this->render('produit/edit.html.twig', [
+                'produit'=>$produit
+            ]);
+        }
+
+
 
     }
 
